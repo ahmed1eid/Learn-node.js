9143c9bf5f60564ae95e7f7042a4405a6d0c37ab
ahmed1eid <eid93461@gmail.com>
2026-03-30 22:44:55 +0200

    use res.sendFile

--- index.js
+++ index.js
@@ -2,17 +2,22 @@ const express = require("express")
 
 const app = express()
 
+app.use(express.json());
+
 app.get("/",(req,res) => {
     res.send("main")
 })
 
 app.post("/req", (req, res) => {
     res.json({
-        url: "req.url",
-        method: "req.method",
-        headers: 'req.headers',
-        query: "req.query",
-        ip: "req.ip",
+        url: req.url,
+        method: req.method,
+        headers:req.headers,
+        query: req.query,
+        ip: req.ip,
+        num1: req.body.num1,
+        num2: req.body.num2,
+        age: req.query.age
     })
 })
 
@@ -20,23 +25,63 @@ app.put("/server",(req,res) => {
     res.send("myserver")
 })
 
-let numders = []
-app.get("/numders",(req,res) => {
 
+app.get("/numbers",(req,res) => {
+    let numbers = []
     for(let i =0 ;i <= 100 ; i++ ){
-        numders = [...numders,i]
+        numbers.push(i)
     }
 
-    res.send(numders)
+    res.send(`
+        <ul>
+         ${numbers.map(num => `<li>${num}</li>`).join('')}
+        </ul>
+    `)
 })
 
 app.delete("/delete",(req,res)=>{
     res.send("delete")
 })
 
-app.delete("/delete",(req,res)=>{
-    res.send("delete")
+app.get("/request_paramiter/getSum/:num1/:num2",(req,res)=>{
+    console.log(req.params)
+    const n1 = parseInt(req.params.num1);
+    const n2 = parseInt(req.params.num2);
+    const result = n1 + n2;
+    res.send(`The summation is: ${result}`);
+
 })
+
+app.post("/body_paramiter/getSub",(req,res)=>{
+    console.log(req.body)
+    const n1 = parseInt(req.body.num1);
+    const n2 = parseInt(req.body.num2);
+    const result = n1 - n2;
+    res.send(`The subtration is: ${result}`);
+
+})
+
+app.get("/quary_paramiter/getmulti",(req,res)=>{
+    console.log(req.query)
+    const n1 = parseInt(req.query.num1);
+    const n2 = parseInt(req.query.num2);
+    const result = n1 * n2;
+    res.send(`The multiplication is: ${result}`);
+
+})
+
+app.get("/quary_paramiter/getdiv",(req,res)=>{
+    console.log(req.query)
+    const n1 = parseInt(req.query.num1);
+    const n2 = parseInt(req.query.num2);
+    const result = n1 / n2;
+    res.send(`The division is: ${result}`);
+});
+
+app.get("/MyPage",(req,res)=>{
+    res.render("MyPage.ejs");
+});
+
 
 let port = 80
 
